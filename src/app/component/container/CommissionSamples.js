27e3609f8ad58316a98b185d04083238b4e64e91
Paulo <pauloreeve143@gmail.com>
2026-05-05 00:48:53 +0800

    fix: samples page responsive padding

--- src/app/component/container/CommissionSamples.js
+++ src/app/component/container/CommissionSamples.js
@@ -2,7 +2,7 @@ import SampleImage from "../card/Sample-Image";
 
 export default function CommissionSamples() {
     return (
-    <div className="columns-2 md:columns-3 lg:columns-4 gap-4 space-y-4 p-4"> 
+    <div className="columns-2 md:columns-3 lg:columns-4 gap-1 md:gap-4 space-y-4 p-1 md:p-4"> 
         <SampleImage src="/arts/comm-1.png"/>
         <SampleImage src="/arts/comm-2.png"/>
         <SampleImage src="/arts/comm-3.png"/>
